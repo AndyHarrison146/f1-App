12ea6d45297bc6680da74cd67d018586a00b04f8
AndyHarrison146 <andyg146@googlemail.com>
2024-03-30 17:45:58 +0000

    styling fix

--- src/components/LastRace.jsx
+++ src/components/LastRace.jsx
@@ -79,9 +79,11 @@ const LastRace = () => {
                     <div className="position-bottom">
                       <div className="team">
                         {screenSize < 600 ? (
-                          <Typography component={"span"} variant="h5">
-                            {Constructor.name}
-                          </Typography>
+                          <div>
+                            <Typography component={"span"} variant="h5">
+                              {Constructor.name}
+                            </Typography>
+                          </div>
                         ) : (
                           <img
                             src={teamImg}
